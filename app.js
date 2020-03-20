5b885bf7b4c890337c7751aea53ee88d3420b090
Jamie Gobeille <djent34@gmail.com>
2020-03-20 15:09:41 +0900

    modified port on app.js

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.use((err, _req, res, _next) => {
 });
 
 // App listen on Port 3000
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log('The app has started!');
 });
 
